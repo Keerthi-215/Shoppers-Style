815b5de85aa39542ab7fd2fe3b7a15245215ed02
Jayalakshmi <p.jayalakshmi3@gmail.com>
2025-03-10 19:18:52 +0100

    upadtedorders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,6 +2,7 @@ import Order from "../models/Order.js";
 
 const getAllOrders = async (req, res) => {
   try {
+    console.log();
     const orders = await Order.find().populate("userId", "name email");
     res.json(orders);
   } catch (error) {
@@ -11,11 +12,49 @@ const getAllOrders = async (req, res) => {
 
 const createOrder = async (req, res) => {
   try {
+    console.log(req.body);
+    console.log(req.user);
+
     const order = await Order.create(req.body);
     res.status(201).json(order);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
+const updateOrder = async (req, res) => {
+  try {
+    const { orderId } = req.params;
+    const updates = req.body;
+    console.log(req.body);
+    console.log(req.user);
 
-export { getAllOrders, createOrder };
+    const updatedPost = await Order.findByIdAndUpdate(orderId, updates, {
+      new: true,
+      runValidators: true,
+    }).populate("userId", "name email");
+    // const orders = await Order.find({_id:orderId}).populate("userId", "name email");
+    // const newOrder= new Order{
+    //   name:name,
+    //   price:price,
+    // }
+    // // const order = await Order.create(req.body);
+    // await newOrder.save()
+    res.status(201).json(newOrder);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+const deleteOrder = async (req, res) => {
+  try {
+    const { orderId } = req.params;
+    console.log(req.body);
+    console.log(req.user);
+    const deletedPost = await Order.findByIdAndDelete(orderId);
+
+    res.status(201).json({ message: "order is deleted!" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+export { getAllOrders, createOrder, updateOrder, deleteOrder };
