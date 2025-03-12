8115c932f1789996b074078debc75bd98a210941
Jayalakshmi <p.jayalakshmi3@gmail.com>
2025-03-12 14:28:21 +0100

    urlrectified

--- client/src/components/OrderList.jsx
+++ client/src/components/OrderList.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://shoppers-bsxp.onrender.com/api";
 
 function OrderList() {
   const [orders, setOrders] = useState([]);
